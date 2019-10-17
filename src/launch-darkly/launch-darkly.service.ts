932f93f1603a51d0861d2a40d06e6d793e9bceea
Vincent Chow <vincent@gromm.co>
2019-10-17 23:07:42 +0800

    Implement launchDarkly service

--- src/launch-darkly/launch-darkly.service.ts
+++ src/launch-darkly/launch-darkly.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import * as config from 'config';
+import * as LaunchDarkly from 'launchdarkly-node-server-sdk';
 
 @Injectable()
-export class LaunchDarklyService {}
+export class LaunchDarklyService {
+  private readonly ldClient: LaunchDarkly.LDClient;
+  private readonly user = {
+    firstName: 'Vincent',
+    lastName: 'Chow',
+    key: 'vincent.chow.demo.key',
+    custom: {
+      groups: 'beta_testers',
+    },
+  };
+
+  constructor() {
+    this.ldClient = LaunchDarkly.init(config.get('LaunchDarkly.key'));
+    this.ldClient.waitForInitialization().catch(error => console.error(error));
+  }
+
+  async getFeatureFlag(flagName: string): Promise<any> {
+    return await this.ldClient.variation(flagName, this.user, false);
+  }
+}
